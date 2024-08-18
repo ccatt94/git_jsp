54c766c3a80a78bc2666e44d03d56abfe607fb48
SHIM <sally940828@naver.com>
2024-08-18 21:00:37 +0900

    8월 18일 홈학습

--- sevlet_lotto/src/main/java/LottoServlet.java
+++ sevlet_lotto/src/main/java/LottoServlet.java
@@ -1,7 +1,8 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,58 +10,74 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LottoServlet
- */
-@WebServlet("/lotto")
+import edu.ict.lotto.Lotto;
+
+@WebServlet("/lottoResult")
 public class LottoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LottoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public LottoServlet() {
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		
+
 		int num1 = Integer.valueOf(request.getParameter("num1"));
 		int num2 = Integer.valueOf(request.getParameter("num2"));
 		int num3 = Integer.valueOf(request.getParameter("num3"));
 		int num4 = Integer.valueOf(request.getParameter("num4"));
 		int num5 = Integer.valueOf(request.getParameter("num5"));
 		int num6 = Integer.valueOf(request.getParameter("num6"));
-		
-		
+
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		
+
+		Set<Integer> userLotto = new HashSet<>();
+		Set<Integer> comLotto = new HashSet<>();
+		Set<Integer> sameLotto = new HashSet<>();
+		Set<Integer> differLotto = new HashSet<>();
+
+		userLotto.add(num1);
+		userLotto.add(num2);
+		userLotto.add(num3);
+		userLotto.add(num4);
+		userLotto.add(num5);
+		userLotto.add(num6);
+
+		while (comLotto.size() < 6) {
+			int num = (int) (Math.random() * 45 + 1);
+			comLotto.add(num);
+		}
+
+		differLotto.addAll(comLotto);
+
+		for (Integer userNum : userLotto) {
+
+			if (comLotto.contains(userNum)) {
+				sameLotto.add(userNum);
+				differLotto.remove(userNum);
+			}
+
+		}
+
 		out.print("<html>");
 		out.print("<body>");
-		
-		
-		
-		
-		
+
+		out.print("입력한 로또 번호 : " + userLotto + "<br>");
+		out.print("랜덤 로또 번호 : " + comLotto.toString() + "<br>");
+		out.print("맞은 번호 : " + sameLotto + "<br>");
+		out.print("틀린 번호 : " + differLotto + "<br>");
+
 		out.print("</body>");
 		out.print("</html>");
-		
-		
-		
+
 	}
 
 }
--- sevlet_lotto/src/main/java/edu/ict/lotto/Lotto.java
+++ sevlet_lotto/src/main/java/edu/ict/lotto/Lotto.java
@@ -1,7 +1,29 @@
 package edu.ict.lotto;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class Lotto {
-	private int num1, num2;
+	
+	public static final int COUNT = 6;
+	public Set<Integer> lottoSet;
+	
+	
+	public Lotto(){
+		
+		lottoSet = new HashSet<>();
+		
+		while(lottoSet.size() < COUNT) {
+			int num = (int)(Math.random()*45+1);
+			lottoSet.add(num);
+		}
+	
+	}
+	
+	public Set<Integer> getLottoSet(){
+		return lottoSet;
+	}
 
+
+	
 }
